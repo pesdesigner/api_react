2965423b23a7f20578ca4207b51ec91f95f4c483
pesdesigner <pauloeduardo.dg@outlook.com>
2020-10-31 01:01:06 -0300

    Conexão com banco MongoDB
    Criação do banco de dados e testes de requisição

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
+const cors = require('cors');
 
 require('./models/home');
 const Home = mongoose.model('Home');
 
-mongoose.connect('mongodb://localhost/app_react', {
+const app = express();
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-PINGOTHER, Content-Type, Authorization');
+    app.use(cors());
+    next();
+})
+
+mongoose.connect('mongodb://localhost/app', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
@@ -15,11 +26,29 @@ mongoose.connect('mongodb://localhost/app_react', {
 });
 
 app.get('/home', (req, res) => {
+    Home.findOne({}).then((home) => {
+        return res.json(home);
+    }).catch((err) => {
+        return res.json({
+            error: false,
+            message: 'Nenhum registro encontrado'
+        });
+    })
+
+});
+
+app.post('/home', (req, res) => {
+    Home.create(req.body, (err) => {
+        if(err) return res.status(400).json({
+            error: true,
+            message: 'Erro: conteúdo não pode ser cadastrado'
+        })
+    })
     return res.json({
         error: false,
-        message: 'informações da página home'
-    });
-}) 
+        message: 'conteúdo cadastrado com sucesso!'
+    })
+})
 
 app.listen(8080, () => {
     console.log("starting at the port 8080");
